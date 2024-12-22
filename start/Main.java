b822151276ac1325bf2118623040e32a80534530
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-12-22 15:28:38 -0500

    string

---
+++ start/Main.java
@@ -0,0 +1,60 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+
+// datatypes
+// public class Main {
+//     public static void main(String[] args) {
+//          // privite datatypes
+         
+//          // byte
+//          // short - 2
+//          // int - 4
+//          // long - 8
+//          // float - 4
+//          // double - 8
+//          // char - 2
+//          // boolean - 1
+
+//          // Non - primitive types
+//         //  strings
+//     }
+// }
+
+
+// string inbuilt functions
+
+// public class Main {
+//     public static void main(String[] args) {
+//         // length
+//         // String name = "luffy";
+//         // System.out.println(name.length());
+
+//         // concatenate
+
+//         // String name1 = "Luffy";
+//         // String name2 = "Zoro";
+//         // String nameadd = name1 + "and" + name2;
+//         // System.out.println(nameadd);
+
+//         // char At
+//         //   String name1 = "Luffy";
+//         //   System.out.println(name1.charAt(0));
+
+//         // replace 
+//         // String are immutable in java we can change orignal string
+
+//         // String name = "Luffy";
+//         // String name2 = name.replace('y', 'Z');
+//         // System.out.println(name2);
+
+//         // substring;
+
+//         // String name = "Nishant Bhai";
+//         // System.out.println(name.substring(0,7));
+
+//     }
+// }
