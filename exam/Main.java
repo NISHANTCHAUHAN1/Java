e3ccb63dcec8a3b56316af1092513b756db9aaea
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-12-25 01:20:49 -0500

    oops inheritance

---
+++ exam/Main.java
@@ -0,0 +1,44 @@
+// class Car {
+//     String brand;
+
+//     Car(String brand) {
+//         this.brand = brand;
+//     }
+
+//     void display() {
+//         System.out.println("Car Brand: " + brand);
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         new Car("Toyota").display();
+//     }
+// }
+
+
+// Inheritance 
+// 1. Single Inheritance
+
+// superClass
+// class Animal {
+//     int add(int a, int b) {
+//         return a+b;
+//     }
+// }
+
+
+// // subclass inherit from superclass;
+// class Dog extends Animal {
+//     int sub(int a, int b) {
+//         return a - b;
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args){
+//         Dog dog = new Dog();
+//         System.out.println(dog.add(3,4));
+//         System.out.println(dog.sub(7,2));
+//     }
+// }
